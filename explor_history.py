d552616e7fbf19cc998bbf3c3f6fe5faa60aa694
suuffernow <suuffer.now@gmail.com>
2025-09-13 20:31:45 +0200

    Add files via upload

--- explor_history.py
+++ explor_history.py
@@ -1,12 +1,15 @@
 from datetime import date
-import psycopg2
+import psycopg2, os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 async def explor_history(ctx, level, result):
     print(f" id: {ctx.author.id} || name: {ctx.author.name} || date: {date.today()} || level: {level} || result: {result[2]} / {result[0]}")
     try:
         if result[2]:
             print(f"{ctx.author.name} - history - add fight status to DB")
-            with psycopg2.connect(dbname="Discord_DDC", user="postgres", password="1234") as conn:
+            with psycopg2.connect(DATABASE_URL) as conn:
                 with conn.cursor() as cur:
                     cur.execute("INSERT INTO explo_history (player_id, player_name, date, level, enemy, status) VALUES (%s, %s,%s, %s, %s, %s)",
                                 (ctx.author.id, ctx.author.name, date.today(), level, result[2], result[0]))
--- userExistCheck.py
+++ userExistCheck.py
@@ -1,8 +1,11 @@
-import psycopg2
+import psycopg2, os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 def userExist(Discord_ID):
     #check if user exist in DB
-    with psycopg2.connect(dbname="Discord_DDC", user="postgres", password="1234") as conn:
+    with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM ddc_player WHERE player_id = %s", [Discord_ID])
             if cur.fetchone() == None:
